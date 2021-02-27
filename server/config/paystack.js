354230f6913fd90604c74df33cf367b46d73d22f
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2021-02-27 10:41:53 +0100

    Initial commit

---
+++ server/config/paystack.js
@@ -0,0 +1,98 @@
+import request from "request"
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//sk_test_xxxx to be replaced by your own secret key/
+const MySecretKey = `Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`;
+
+const paystack = {
+  // POST REQUEST
+  async initializePayment(form, mycallback) {
+    const options = {
+      form,
+      url : 'https://api.paystack.co/transaction/initialize',
+      headers : {
+        authorization: MySecretKey,
+        'content-type': 'application/json',
+        'cache-control': 'no-cache'
+      }
+    }
+
+    const callback = (error, response, body)=>{
+      return mycallback(error, body);
+    }
+
+    return request.post(options, callback);
+  },
+
+  // GET REQUEST
+  async verifyPayment(ref, mycallback) {
+    const options = {
+      url : 'https://api.paystack.co/transaction/verify/' + encodeURIComponent(ref),
+      headers : {
+        authorization: MySecretKey,
+        'content-type': 'application/json',
+        'cache-control': 'no-cache'
+      }
+    }
+
+    const callback = (error, response, body) => {
+      return mycallback(error, body);
+    }
+
+    return request(options, callback);
+  }
+}
+
+
+
+
+// const paystack = () => {
+//   //sk_test_xxxx to be replaced by your own secret key
+//   const MySecretKey = `Bearer ${process.env.PAYSTACK_TEST_SECRET_KEY}`;
+  
+//   // POST REQUEST
+//   const initializePayment = (form, mycallback) => {
+//     const options = {
+//       form,
+//       url : 'https://api.paystack.co/transaction/initialize',
+//       headers : {
+//         authorization: MySecretKey,
+//         'content-type': 'application/json',
+//         'cache-control': 'no-cache'
+//       }
+//     }
+
+//     const callback = (error, response, body)=>{
+//       return mycallback(error, body);
+//     }
+
+//     return request.post(options, callback);
+//   }
+
+//   // GET REQUEST
+//   const verifyPayment = (ref, mycallback) => {
+//     const options = {
+//       url : 'https://api.paystack.co/transaction/verify/' + encodeURIComponent(ref),
+//       headers : {
+//         authorization: MySecretKey,
+//         'content-type': 'application/json',
+//         'cache-control': 'no-cache'
+//       }
+//     }
+
+//     const callback = (error, response, body) => {
+//       return mycallback(error, body);
+//     }
+
+//     return request(options, callback);
+//   }
+
+//   return {
+//     initializePayment, 
+//     verifyPayment
+//   };
+// }
+
+export default paystack
---
+++ server/models/models.js
@@ -0,0 +1,23 @@
+import mongoose from "../config/mongoose"
+
+const donorSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  reference: {
+    type: String,
+    required: true
+  }
+});
+const Donor = mongoose.model('Donor', donorSchema);
+
+export default Donor
